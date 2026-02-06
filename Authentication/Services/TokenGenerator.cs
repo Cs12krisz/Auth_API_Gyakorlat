0c36107d0b167aa6483386af06d93588d478112b
Cs12krisz <csigerkrisztian@gmail.com>
2026-02-06 09:37:45 +0100

    A token létrehozó metódus elkészült

--- Authentication/Services/TokenGenerator.cs
+++ Authentication/Services/TokenGenerator.cs
@@ -1,13 +1,49 @@
 ﻿using Authentication.Models;
+using Authentication.Models.Dtos;
 using Authentication.Services.AuthInterfaces;
+using K4os.Compression.LZ4.Engine;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Authentication.Services
 {
     public class TokenGenerator : ITokenGenerator
     {
+        private readonly JwtOptions jwtOptions;
+
+        public TokenGenerator(IOptions<JwtOptions> jwtOptions)
+        {
+            this.jwtOptions = jwtOptions.Value;
+        }
+
         public string GenerateToken(ApplicationUser applicationUser, IEnumerable<string> roles)
         {
-            throw new NotImplementedException();
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(jwtOptions.Secret);
+
+            var claimList = new List<Claim> 
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, applicationUser.Id),
+                new Claim(JwtRegisteredClaimNames.Name, applicationUser.UserName.ToString()),
+            };
+            claimList.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+            var tokenDescription = new SecurityTokenDescriptor()
+            {
+                Audience = jwtOptions.Audience,
+                Issuer = jwtOptions.Issuer,
+                Subject = new ClaimsIdentity(claimList),
+                Expires = DateTime.Now.AddHours(6),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescription);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
